ffa03590c1f79c90a2117809e7509b955a75567a
dylanbrass <dylan.brassard@outlook.com>
2025-11-20 23:10:04 -0500

    Removed unused import

--- FE/src/app.js
+++ FE/src/app.js
@@ -1,14 +1,13 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
-import { PageLoader } from "./components/page-loader";
-import { AuthenticationGuard } from "./components/authentication-guard";
-import { AdminPage } from "./pages/admin-page";
-import { CallbackPage } from "./pages/callback-page";
-import { HomePage } from "./pages/home-page";
-import { NotFoundPage } from "./pages/not-found-page";
-import { ProfilePage } from "./pages/profile-page";
-import { ProtectedPage } from "./pages/protected-page";
-import { PublicPage } from "./pages/public-page";
+import {Route, Routes} from "react-router-dom";
+import {AuthenticationGuard} from "./components/authentication-guard";
+import {AdminPage} from "./pages/admin-page";
+import {CallbackPage} from "./pages/callback-page";
+import {HomePage} from "./pages/home-page";
+import {NotFoundPage} from "./pages/not-found-page";
+import {ProfilePage} from "./pages/profile-page";
+import {ProtectedPage} from "./pages/protected-page";
+import {PublicPage} from "./pages/public-page";
 import {useAuth} from "react-oidc-context";
 
 export const App = () => {
@@ -25,23 +24,23 @@ export const App = () => {
 
 
     return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route
-        path="/profile"
-        element={<AuthenticationGuard component={ProfilePage} />}
-      />
-      <Route path="/public" element={<PublicPage />} />
-      <Route
-        path="/protected"
-        element={<AuthenticationGuard component={ProtectedPage} />}
-      />
-      <Route
-        path="/admin"
-        element={<AuthenticationGuard component={AdminPage} />}
-      />
-      <Route path="/callback" element={<CallbackPage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
-  );
+        <Routes>
+            <Route path="/" element={<HomePage/>}/>
+            <Route
+                path="/profile"
+                element={<AuthenticationGuard component={ProfilePage}/>}
+            />
+            <Route path="/public" element={<PublicPage/>}/>
+            <Route
+                path="/protected"
+                element={<AuthenticationGuard component={ProtectedPage}/>}
+            />
+            <Route
+                path="/admin"
+                element={<AuthenticationGuard component={AdminPage}/>}
+            />
+            <Route path="/callback" element={<CallbackPage/>}/>
+            <Route path="*" element={<NotFoundPage/>}/>
+        </Routes>
+    );
 };
